022bbab1085fc61c4a19937ba0d1cabe2416b18e
zSnails <erizojuan33@gmail.com>
2019-07-24 14:42:12 -0600

    uuf

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const rl = readLine.createInterface(process.stdin, process.stdout);
 const fs = require('fs');
 const argv = require('yargs').argv
 let conf = require(`${__dirname}/config.json`);
-
 if (argv.version) {
     const ver = require(`${__dirname}/package.json`);
     console.log(ver)
@@ -30,70 +29,77 @@ if (argv.config) {
         console.log(`There's no save path\nRun 'how --config' to setup a save path`)
         process.exit()
     }
-    let pe = Math.floor(Math.random() * 9999);
-    var file = `${conf.saveto}/howified/how-${pe}.jpeg`
-    rl.question("Provide an url/path to the image that you want to HOW-ify ", async function (answer) {
-        filepath = answer;
-        await rl.close()
-        console.time("⏱️");
-        jimp.read(filepath).then(image => {
-            if (image.bitmap.width & image.bitmap.height < 60) {
-                return console.log(chalk.red("That image is too small"))
-            }
-            console.log(chalk.yellow('Writing image (this may take a while)'))
-            let w;
-            let h;
 
 
-            if (image.bitmap.width && image.bitmap.height < 500) {
-                w = image.bitmap.width - 100;
-                h = image.bitmap.height - 100;
+    fs.readdir(`${conf.saveto}/howified/`, (err, files) => {
+        if (err) return console.error(err);
+        let ar = files.filter(f => f.split(".").pop() === "jpeg");
+        let pito = ar.length + 1
 
-            } else if (image.bitmap.width && image.bitmap.height < 200) {
-                w = image.bitmap.width - 50;
-                h = image.bitmap.height - 50;
+        var file = `${conf.saveto}/howified/how-(${pito}).jpeg`
+        rl.question("Provide an url/path to the image that you want to HOW-ify ", async function (answer) {
+            filepath = answer;
+            await rl.close()
+            console.time("⏱️");
+            jimp.read(filepath).then(image => {
+                if (image.bitmap.width & image.bitmap.height < 60) {
+                    return console.log(chalk.red("That image is too small"))
+                }
+                console.log(chalk.yellow('Writing image (this may take a while)'))
+                let w;
+                let h;
 
-            } else {
-                w = image.bitmap.width - 400;
-                h = image.bitmap.height - 400;
-            }
 
-            image.resize(w, h)//.catch(err => console.error(chalk.red("Error: The image is too small")))
-            let wea = image.bitmap.width / 2.8
+                if (image.bitmap.width && image.bitmap.height < 500) {
+                    w = image.bitmap.width - 100;
+                    h = image.bitmap.height - 100;
 
-            let fSize;
-            let ver;
-            if (image.bitmap.width < 270) {
-                fSize = `${__dirname}/impact/impact_32.fnt`
-                ver = -20
-            } else {
-                fSize = `${__dirname}/impact/impact.fnt`
-                ver = -70
-            }
-            jimp.loadFont(fSize).then(font => {
+                } else if (image.bitmap.width && image.bitmap.height < 200) {
+                    w = image.bitmap.width - 50;
+                    h = image.bitmap.height - 50;
 
-                image.color([
-                    { apply: 'saturate', params: [20] },
-                    { apply: 'xor', params: [40] }
-                ]).print(
-                    font,
-                    wea,
-                    ver,
-                    "HOW",
-                    50
-                )
-                    .posterize(100)
+                } else {
+                    w = image.bitmap.width - 400;
+                    h = image.bitmap.height - 400;
+                }
 
-                    .quality(10)
+                image.resize(w, h)//.catch(err => console.error(chalk.red("Error: The image is too small")))
+                let wea = image.bitmap.width / 2.8
 
-                    .write(file);
+                let fSize;
+                let ver;
+                if (image.bitmap.width < 270) {
+                    fSize = `${__dirname}/impact/impact_32.fnt`
+                    ver = -20
+                } else {
+                    fSize = `${__dirname}/impact/impact.fnt`
+                    ver = -70
+                }
+                jimp.loadFont(fSize).then(font => {
 
-                let eu = `Width: ${image.bitmap.width}\nHeight: ${image.bitmap.height}`
-                console.log(chalk.green('Finished writing image'));
-                console.log(eu)
-                console.timeEnd("⏱️")
-            })
-        })//.catch(err => console.error(chalk.red(`Error: ${err.message}`)))
-    });
+                    image.color([
+                        { apply: 'saturate', params: [20] },
+                        { apply: 'xor', params: [40] }
+                    ]).print(
+                        font,
+                        wea,
+                        ver,
+                        "HOW",
+                        50
+                    )
+                        .posterize(100)
 
+                        .quality(10)
+
+                        .write(file);
+
+                    let eu = `Width: ${image.bitmap.width}\nHeight: ${image.bitmap.height}`
+                    console.log(chalk.green('Finished writing image'));
+                    console.log(eu)
+                    console.timeEnd("⏱️")
+                })
+            })//.catch(err => console.error(chalk.red(`Error: ${err.message}`)))
+        });
+
+    })
 }

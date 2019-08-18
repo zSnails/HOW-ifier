00f35548899dfa3752b81444e1daae1c470ceaf9
zSnails <erizojuan33@gmail.com>
2019-08-17 21:00:14 -0300

    hey

--- index.js
+++ index.js
@@ -1,113 +1,97 @@
 #!/usr/bin/env node
 const logger = require('./util/logger.js');
 const jimp = require('jimp');
-const readLine = require('readline');
 let filepath;
 const chalk = require('chalk');
-const rl = readLine.createInterface(process.stdin, process.stdout);
 const fs = require('fs');
-const argv = require('yargs').argv
+const argv = process.argv.slice(2)
 let conf = require(`${__dirname}/config.json`);
-if (argv.version) {
-    const ver = require(`${__dirname}/package.json`);
-    logger.info(ver)
+
+let temp = `Usage:
+    how <Url|Image path>
+    
+Flags:
+    -v | --version
+    `
+
+
+if (!argv[0]) {
+    logger.error('Wrong usage')
+    console.log(temp)
+    process.exit()
 }
-if (argv.config) {
-    logger.warn("If you're on windows use / instead of \\")
-    rl.question("Provide the desired save path: ", async function (answer) {
-        let temp = `{"saveto": "${answer}"}`
-        fs.readdir(answer, async function (err, files) {
-            if (err) return logger.error("That's not a valid path")
-            else {
-                fs.writeFileSync(`${__dirname}/config.json`, temp);
-                logger.info(`New save path set to ${answer}`)
-
-            }
-
-        });
-
-
-        fs.mkdir(`${answer}/howified/`, (err) => {
-            if (err) return;
-        })
-        await rl.close()
-    })
+if (argv[0] === '--version' || argv[0] === '-v') {
+    const ver = require(`${__dirname}/package.json`);
+    logger.info(ver.version)
+    process.exit()
 } else {
-    if (conf.saveto === 'nicepath') {
-        logger.info(`There's no save path. Run 'how --config' to setup a save path`)
-        process.exit()
-    }
-
-    fs.readdir(`${conf.saveto}/howified/`, (err, files) => {
+    fs.readdir(`${process.cwd()}`, (err, files) => {
         let ar = files.filter(f => f.split(".").pop() === "jpeg");
         let pito = ar.length + 1
 
-        var file = `${conf.saveto}/howified/how-(${pito}).jpeg`
-        rl.question("Provide an url/path to the image that you want to HOW-ify ", async function (answer) {
-            filepath = answer;
-            await rl.close()
-            console.time(`[${new Date().toLocaleDateString()}]:[INFO]`);
-            jimp.read(filepath).then(image => {
-                if (image.bitmap.width && image.bitmap.height < 60) {
-                    return logger.error(chalk.red("That image is too small"))
-                }
-                logger.info(chalk.yellow('Writing image (this may take a while)'))
-                let w;
-                let h;
+        var file = `${process.cwd()}/how-(${pito}).jpeg`
+        filepath = argv[0];
+        console.time(`[${new Date().toLocaleDateString()}]:[INFO]`);
+        jimp.read(filepath).then(image => {
+            if (image.bitmap.width && image.bitmap.height < 60) {
+                return logger.error(chalk.red("That image is too small"))
+            }
+            logger.info(chalk.yellow('Writing image (this may take a while)'))
+            let w;
+            let h;
 
 
-                if (image.bitmap.width && image.bitmap.height < 500) {
-                    w = image.bitmap.width - 100;
-                    h = image.bitmap.height - 100;
+            if (image.bitmap.width && image.bitmap.height < 500) {
+                w = image.bitmap.width - 100;
+                h = image.bitmap.height - 100;
 
-                } else if (image.bitmap.width && image.bitmap.height < 150) {
-                    w = image.bitmap.width - 50;
-                    h = image.bitmap.height - 50;
+            } else if (image.bitmap.width && image.bitmap.height < 150) {
+                w = image.bitmap.width - 50;
+                h = image.bitmap.height - 50;
 
-                } else {
-                    w = image.bitmap.width - 400;
-                    h = image.bitmap.height - 400;
-                }
-                try {
-                    image.resize(w, h)
-                } catch (err) {
-                    return logger.error(err)
-                }
+            } else {
+                w = image.bitmap.width - 400;
+                h = image.bitmap.height - 400;
+            }
+            try {
+                image.resize(w, h)
+            } catch (err) {
+                return logger.error(err)
+            }
 
-                let wea = image.bitmap.width / 2.8
+            let wea = image.bitmap.width / 2.8
 
-                let fSize;
-                let ver;
-                if (image.bitmap.width < 270) {
-                    fSize = `${__dirname}/impact/impact_32.fnt`
-                    ver = -20
-                } else {
-                    fSize = `${__dirname}/impact/impact.fnt`
-                    ver = -70
-                }
-                jimp.loadFont(fSize).then(font => {
+            let fSize;
+            let ver;
+            if (image.bitmap.width < 270) {
+                fSize = `${__dirname}/impact/impact_32.fnt`
+                ver = -20
+            } else {
+                fSize = `${__dirname}/impact/impact.fnt`
+                ver = -70
+            }
+            jimp.loadFont(fSize).then(font => {
 
-                    image.color([
-                        { apply: 'saturate', params: [20] },
-                        { apply: 'xor', params: [40] }
-                    ]).print(
-                        font,
-                        wea,
-                        ver,
-                        "HOW",
-                        50
-                    )
-                        .posterize(100)
-                        .quality(10)
-                        .write(file);
-                    logger.info(chalk.green('Finished writing image'));
-                    logger.info(`Width: ${image.bitmap.width}`)
-                    logger.info(`Height: ${image.bitmap.height}`)
-                    console.timeEnd(`[${new Date().toLocaleDateString()}]:[INFO]`)
+                image.color([
+                    { apply: 'saturate', params: [20] },
+                    { apply: 'xor', params: [40] }
+                ]).print(
+                    font,
+                    wea,
+                    ver,
+                    "HOW",
+                    50
+                )
+                    .posterize(100)
+                    .quality(10)
+                    .write(file);
+                logger.info(chalk.green('Finished writing image'));
+                logger.info(`Width: ${image.bitmap.width}`)
+                logger.info(`Height: ${image.bitmap.height}`)
+                console.timeEnd(`[${new Date().toLocaleDateString()}]:[INFO]`)
+            })
+        })//.catch(err => console.error(chalk.red(`Error: ${err.message}`)))
 
-                })
-            })//.catch(err => console.error(chalk.red(`Error: ${err.message}`)))
-        });
 
     })
 
